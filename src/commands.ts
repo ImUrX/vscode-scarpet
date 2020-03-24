73672dc4c1bee9c63b63ee073033693e623e19ef
ImUrX <urielfontan2002@gmail.com>
2020-03-24 15:52:01 -0300

    remove ALL comments

--- src/commands.ts
+++ src/commands.ts
@@ -2,23 +2,18 @@
 import * as vscode from "vscode";
 
 export function copyClipboard({ document }: vscode.TextEditor) {
-    const clipboard = [];
-    for(let i = 0; i < document.lineCount; i++) {
-        const text = document.lineAt(i).text;
+  const clipboard = [];
+  for (let i = 0; i < document.lineCount; i++) {
+    const text = document.lineAt(i).text.replace(/\/\/.*/, "");
 
-        if(/^\s*\/\//.test(text)) {
-            continue;
-        }
-
-        if(text.replace(/\/\/.*/, "").startsWith("$")) {
-            clipboard.push(text);
-        } else {
-            if(text.replace(/\/\/.*/,"")=="\n" ||  text.replace(/\/\/.*/,"")==""){
-                clipboard.push(text);
-            } else {
-                clipboard.push("$" + text);
-            }
-        }
+    if (/^\s*\/\//.test(text)) {
+      continue;
     }
-    vscode.env.clipboard.writeText(clipboard.join("\n"));
+    if (text.startsWith("$")) {
+      clipboard.push(text);
+    } else {
+      clipboard.push("$" + text);
+    }
+  }
+  vscode.env.clipboard.writeText(clipboard.join("\n"));
 }
