880a02fc607359319fd814944161cbce3cbf1be9
ImUrX <urielfontan2002@gmail.com>
2020-03-25 08:05:24 -0300

    update command

--- src/commands.ts
+++ src/commands.ts
@@ -4,11 +4,11 @@ import * as vscode from "vscode";
 export function copyClipboard({ document }: vscode.TextEditor) {
   const clipboard = [];
   for (let i = 0; i < document.lineCount; i++) {
-    const text = document.lineAt(i).text.replace(/\/\/.*/, "");
-
-    if (/^\s*\/\//.test(text)) {
+    if (/^\s*(\/\/|$)/.test(document.lineAt(i).text)) {
       continue;
     }
+    const text = document.lineAt(i).text.replace(/\/\/.*/, "");
+
     if (text.startsWith("$")) {
       clipboard.push(text);
     } else {
